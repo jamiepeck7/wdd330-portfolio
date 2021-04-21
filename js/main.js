056a9d95819802c29d99516678ade05a155d21d8
Use your Github username <Enter your email address>
2021-04-20 22:45:31 -0600

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const links = [
     {
-    label: "Week1 notes",
+    label: "Week 1",
     url: "week1/index.html"
     }
 ]
+
+for (let i=0; i < links.length; i ++) {
+    let link = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let h3 = document.createElement('h3');
+
+    h2.textContent = links[i].label;
+    h3.innerHTML = "<a href = " + links[i].url + ">Notes</a>";
+
+    link.appendChild(h2);
+    link.appendChild(h3);
+
+    document.querySelector('div.link').appendChild(link);
+}
