1767829133e232d6f5be4f200dd9646959fb3ff5
Use your Github username <Enter your email address>
2021-06-29 10:50:15 -0600

    commit

--- week10/team/js/index.js
+++
@@ -1,9 +0,0 @@
-import QuakesController from './QuakesController.js';
-
-import buildNavigation from './routing.js';
-
-const navElement = document.getElementById('mainNav');
-buildNavigation(navElement);
-
-const quakesController = new QuakesController('#quakeList');
-quakesController.init();
--- week10/team/quakes/js/Quake.js
+++ week10/team/quakes/js/Quake.js
@@ -1,4 +1,6 @@
-import {getJSON} from './utilities.js';
+import {
+    getJSON
+} from './utilities.js';
 // Quake Model
 export default class Quake {
     constructor() {
@@ -7,7 +9,7 @@ export default class Quake {
         // this is where we will store the last batch of retrieved quakes in the model.  I don't always do this...in this case the api doesn't have an endpoint to request one quake.
         this._quakes = [];
     }
-    async getEarthQuakesByRadius(position, radius = 100) {
+    async getEarthQuakesByRadius(position, radius) {
         // use the getJSON function and the position provided to build out the correct URL to get the data we need.  Store it into this._quakes, then return it
         const query = this.baseUrl + `&latitude=${position.lat}&longitude=${position.lon}&maxradiuskm=${radius}`;
         console.log(query);
